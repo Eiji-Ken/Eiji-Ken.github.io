2dc3238d4d94f5810afb2fa0bca0bc2378cb91d0
Hexianren <1979181470@qq.com>
2025-08-29 21:46:41 +0800

    Site updated: 2025-08-29 21:46:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/15/hello-world/","2025/08/20/测试/"];function toRandomPost(){
+var posts=["2025/08/20/测试/","2025/08/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
