59ed32d38a0f9df84f5ac29d8b639594bff7409b
Hexianren <1979181470@qq.com>
2025-09-18 01:12:47 +0800

    Site updated: 2025-09-18 01:12:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/15/hello-world/","2025/08/24/Redis和Mysql数据一致性/"];function toRandomPost(){
+var posts=["2025/08/29/Redis和Mysql数据一致性/","2025/09/17/消息队列是什么/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
